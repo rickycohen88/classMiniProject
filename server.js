dc372f7df47f7c149b2791f25f8f8238bc4345df
vfavorito <vince.favorito@gmail.com>
2020-12-10 19:42:59 -0600

    added routes

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.listen(PORT, function(){
     console.log("App running on Port " + PORT);
 });
 
-let tables = []
-let waitlist = []
+let tables = [];
+let waitlist = [];
 
 app.get("/",function(req,res){
     res.sendFile(path.join(__dirname,"home.html"));
@@ -22,6 +22,10 @@ app.get("/reserve",function(req,res){
     res.sendFile(path.join(__dirname,"reserve.html"));
 });
 
-app.get("/tables",function(req,res){
-    res.sendFile(path.join(__dirname,"tables.html"));
+app.get("/api/tables",function(req,res){
+    return res.json(tables);
+});
+
+app.get("/api/waitlist",function(req,res){
+    return res.json(waitlist);
 });
