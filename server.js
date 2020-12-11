443e5f25a764ff4db46c7c85dbff6eec05639e2c
vfavorito <vince.favorito@gmail.com>
2020-12-10 19:15:47 -0600

    fixed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ let express =require("express");
 let path = require("path");
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
