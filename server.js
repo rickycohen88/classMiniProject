8401954f6a2823221b8d8d23f834e21f090f4395
vfavorito <vince.favorito@gmail.com>
2020-12-10 19:12:01 -0600

    barebones of server setup

---
+++ server.js
@@ -0,0 +1,10 @@
+let express =require("express");
+let path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.listen(PORT, function {console.log("App running on Port " + PORT)});
